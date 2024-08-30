c0330247b0bde2b94f565b9637ef8b932286388e
Stadrem <stadrem@naver.com>
2024-08-30 16:02:46 +0900

    챗 UI

---
+++ Assets/KDS/Scripts/UiSoundManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiSoundManager : MonoBehaviour
+{
+    public static UiSoundManager instance;
+
+    public AudioClip click;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        //instance 값이 null이면
+        if (instance == null)
+        {
+            //이 스크립트를 instance에 담음
+            instance = this;
+
+            //씬 전환해도 유지하는 코드
+            DontDestroyOnLoad(gameObject);
+        }
+        //이미 instance에 무언가 값이 들어있다면?
+        else
+        {
+            //의도치 않은 중복 적용일 태니 이 게임 오브젝트 파괴.
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void PlaySound(AudioClip audios, float volume)
+    {
+        audioSource.PlayOneShot(audios, volume);
+    }
+
+    public void AudioClick()
+    {
+        PlaySound(click, 0.5f);
+    }
+}
