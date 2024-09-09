4d0bf31023e7a8f6bb5d9d87d8ee1e7a040349ab
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-09-09 10:52:26 +0900

    ai와 통신 처리 완

--- Assets/KSY/01.Scripts/K_EventMethodRef.cs
+++ Assets/KSY/01.Scripts/K_EventMethodRef.cs
@@ -19,19 +19,35 @@ public class K_EventMethodRef : MonoBehaviour
     public class ChatBotAns
     {
         public int id;
-        public string memberName;
-        public string question;
         public string createdAt;
-        public int member_id;
+        public string member_id;
+        public string member_name;
+        public string answer;
         public string area_size;
         public string housemate_num;
     }
+    [System.Serializable]
+    public class ChatBotInput_AI
+    {
+        public string user_id;
+        public string question;
+        public string area_size;
+        public string housemate_num;
+    }
+    [System.Serializable]
+    public class ChatBotAns_AI
+    {
+        public string answer;
+    }
+    
     public ChatBotAns ans;
-
+    public ChatBotAns_AI ansAI;
     public TMP_InputField input;
     public TMP_Text output;
     public TMP_Dropdown items;
 
+    public bool ToAI = true;
+    
     void Start()
     {
         
@@ -50,37 +66,70 @@ public class K_EventMethodRef : MonoBehaviour
     public void TransferInput()
     {
         K_HttpInfo info = new K_HttpInfo();
-        info.url = "http://125.132.216.190:12450/api/talks";
-        
         ChatBotInput chat = new ChatBotInput();
+        ChatBotInput_AI chatAI = new ChatBotInput_AI();
         if (AccountDate.GetInstance().currentInfo.userId == null || AccountDate.GetInstance().currentInfo.userId == "")
         {
             print("userId가 null이거나 빔.");
             chat.member_id = "0";
             chat.area_size = "8평";
             chat.housemate_num = "13";
+            chatAI.user_id = "0";
+            chatAI.area_size = "8평";
+            chatAI.housemate_num = "13";
+
         }
         else
         {
             //chat.member_id = AccountDate.instance.currentInfo.userId;
+            chatAI.user_id = "0";
+            chatAI.area_size = "8평";
+            chatAI.housemate_num = "13";
             chat.member_id = "0";
             chat.area_size = "8평";
             chat.housemate_num = "13";
             print("흠");
         }
         chat.question = input.text;
+        chatAI.question = input.text;
         input.text = "처리중입니다...";
         output.text = "처리중입니다...";
         input.interactable = false;
-        info.token = AccountDate.GetInstance().response.accessToken;
-        info.body = JsonUtility.ToJson(chat);
+        
+        
         Debug.Log(info.body);
         info.contentType = "application/json";
+        if (ToAI)
+        {
+            info.url = "http://meta-ai.iptime.org:8282/ask";
+            info.token = "";
+            info.body = JsonUtility.ToJson(chatAI);
+            print("AI와 통신 중");
+        }
+        else if (!ToAI)
+        {
+            info.body = JsonUtility.ToJson(chat);
+            info.url = "http://125.132.216.190:12450/api/talks";
+            info.token = AccountDate.GetInstance().response.accessToken;
+        }
         info.onComplete = (downloadHandler) => {
             print(downloadHandler.text);
-            ans = JsonUtility.FromJson<ChatBotAns>(downloadHandler.text);
-            print(ans.question);
-            output.text = ans.question;
+            if (!ToAI)
+            {
+                ans = JsonUtility.FromJson<ChatBotAns>(downloadHandler.text);
+                print(ans.answer);
+                ans.answer = ans.answer.Replace("**", "  ");
+                output.text = ans.answer;
+            }
+            else if (ToAI)
+            {
+                ansAI = JsonUtility.FromJson<ChatBotAns_AI>(downloadHandler.text);
+                print(ansAI.answer);
+                ansAI.answer = ansAI.answer.Replace("**", "  ");
+                output.text = ansAI.answer;
+            }
+            
+            
             input.text = "";
             input.interactable = true;
         };
