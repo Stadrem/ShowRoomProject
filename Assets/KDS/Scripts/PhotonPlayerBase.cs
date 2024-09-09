2f4864e4a54c2e3eaee5abafd80f6f86f4c861b6
Stadrem <stadrem@naver.com>
2024-09-09 18:06:49 +0900

    포톤 멀티 연결중

--- Assets/KDS/Scripts/PhotonPlayerBase.cs
+++ Assets/KDS/Scripts/PhotonPlayerBase.cs
@@ -12,12 +12,19 @@ public class PhotonPlayerBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = PhotonNetwork.Instantiate("Player", new Vector3(0, 0, 0), Quaternion.identity);
+        StartCoroutine(SpawnPlayer());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnPlayer()
     {
-        
+        //룸에 입장이 완료될 때까지 기다린다.
+        yield return new WaitUntil(() => { return PhotonNetwork.InRoom; });
+
+        //약간 무작위 공간에 생성
+        Vector2 randomPos = Random.insideUnitCircle * 2.0f;
+        Vector3 initPosition = new Vector3(randomPos.x, 0.0f, randomPos.y);
+
+        //포톤 네트워크 전용 생성기
+        player = PhotonNetwork.Instantiate("Player", initPosition, Quaternion.identity);
     }
 }
