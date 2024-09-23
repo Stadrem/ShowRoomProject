2ad867da832a787c21f77d035e00a819857cede7
Stadrem <stadrem@naver.com>
2024-09-23 09:33:02 +0900

    프리팹 자동 생성

---
+++ Assets/KDS/Scripts/SceneChanged.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanged : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.activeSceneChanged += OnSceneChanged;
+
+        // 씬 전환 시 객체 파괴 방지
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    private void OnSceneChanged(Scene previousScene, Scene newScene)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
