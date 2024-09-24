cfe079b7a64b8334783119a67303f0090d0a23a7
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-09-24 12:41:22 +0900

    Fix Player Camera

--- Assets/KSY/01.Scripts/K_CamRot.cs
+++ Assets/KSY/01.Scripts/K_CamRot.cs
@@ -11,6 +11,7 @@ public class K_CamRot : MonoBehaviourPun
     float my;
     float mx;
     public K_PlayerMove playerMove;
+    GameObject virtualCamPlayer;
 
     private float initialY;
 
@@ -23,14 +24,15 @@ public class K_CamRot : MonoBehaviourPun
         //    Camera.main.transform.localRotation = Quaternion.identity;
         //}
         // 초기 Y 값을 Start에서 한 번만 저장
+        virtualCamPlayer = GameObject.FindWithTag("MainVirtualCam");
         initialY = transform.localEulerAngles.y;
     }
     void Update()
     {
         if (cam && photonView.IsMine)
         {
-            Camera.main.transform.position = transform.position;
-            Camera.main.transform.rotation = transform.rotation;
+            virtualCamPlayer.transform.position = transform.position;
+            virtualCamPlayer.transform.rotation = transform.rotation;
         }
         if (playerMove.currState == K_PlayerMove.PlayerState.Click) return;
         if (!photonView.IsMine) return;
@@ -38,13 +40,15 @@ public class K_CamRot : MonoBehaviourPun
         {
             my -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed;
             my = Mathf.Clamp(my, -40f, 40f);
+            transform.localEulerAngles = new Vector3(my, 0, 0);
             
         }
         else
         {
-            mx += Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed;
+            mx = Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed;
             //transform.localEulerAngles += new Vector3(0, mx * Time.deltaTime* rotSpeed, 0);
+            transform.localEulerAngles += new Vector3(0, mx, 0);
         }
-        transform.localEulerAngles = new Vector3(my, initialY + mx, 0);
+        //transform.localEulerAngles = new Vector3(my, mx, 0);
     }
 }
