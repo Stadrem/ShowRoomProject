4fa97b196f5d28ea239b3e6ae22a3bc73a0ff41e
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-09-25 18:24:53 +0900

    Processing Change Rf`s Doors` Mat

--- Assets/KSY/01.Scripts/K_ObjectControl.cs
+++ Assets/KSY/01.Scripts/K_ObjectControl.cs
@@ -12,69 +12,51 @@ public class K_ObjectControl : MonoBehaviour
     public GameObject rf1;
     public GameObject rf2;
 
-    public int animState_r1 = 0;
-    public int animState_r2 = 0;
-
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            specUI.SetActive(true);
-        }
         if(player != null)
         {
             myUI.transform.forward = player.transform.forward;
             PhotonView photonView = player.GetPhotonView();
         }
-
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            specUI.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (rf1.activeSelf)
+            {
+                rf1.SetActive(false);
+                rf2.SetActive(true);
+            }
+            else if (rf2.activeSelf)
+            {
+                rf2.SetActive(false);
+                rf1.SetActive(true);
+            }
+        }
         if (rf1.activeSelf)
         {
-            rf1.GetComponent<Animator>().SetFloat("OPENCONTROL", animState_r1, 0.1f, Time.deltaTime);
             if (Input.GetKeyDown(KeyCode.R))
             {
-                GetAnimFloat(1);
+                rf1.GetComponent<Animator>().SetTrigger("Door");
             }
         }
         else if (rf2.activeSelf)
         {
-            rf2.GetComponent<Animator>().SetFloat("OPENCONTROL", animState_r2, 0.1f, Time.deltaTime);
             if (Input.GetKeyDown(KeyCode.R))
             {
-                GetAnimFloat(2);
+                rf2.GetComponent<Animator>().SetTrigger("Door");
             }
         }
         if (player == null) player = GameObject.FindWithTag("Player");
-        if (inPlayer)
-        {
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                if (rf1.activeSelf)
-                {
-                    rf1.SetActive(false);
-                    rf2.SetActive(true);
-                }
-                else if (rf2.activeSelf)
-                {
-                    rf2.SetActive(false);
-                    rf1.SetActive(true);
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                specUI.SetActive(true);
-            }
-        }
-        else
-        {
-
-        }
-
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -90,29 +72,6 @@ public class K_ObjectControl : MonoBehaviour
         {
             inPlayer = false;
         }
-    }
-
-    public void GetAnimFloat(int rf_num)
-    {
-        if(rf_num == 1)
-        {
-            animState_r1++;
-            if(animState_r1 > 2)
-            {
-                animState_r1 = 0;
-                rf1.GetComponent<Animator>().SetFloat("OPENCONTROL", 0);
-            }
-        }
-        if(rf_num == 2)
-        {
-            animState_r2++;
-            if (animState_r2 > 2)
-            {
-                animState_r2 = 0;
-                rf2.GetComponent<Animator>().SetFloat("OPENCONTROL", 0);
-            }
-        }
-
     }
 
 }
--- Assets/KSY/01.Scripts/K_UIManager.cs
+++ Assets/KSY/01.Scripts/K_UIManager.cs
@@ -3,6 +3,7 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class K_UIManager : MonoBehaviour
 {
@@ -10,12 +11,62 @@ public class K_UIManager : MonoBehaviour
     public GameObject player;
     public PhotonView playerPV;
     public GameObject img_Aim;
+    public GameObject ui_ObjGuide;
     public GameObject currentUI;
+
+    public SkinnedMeshRenderer rf1_MeshRenderer;
+    public SkinnedMeshRenderer rf2_MeshRenderer;
+    public Material[] mat_Rf1_Doors = new Material[3];
+    public Material[] mat_Rf2_Doors = new Material[4];
+    public Dictionary<Material, Color> dic_DoorsColor = new Dictionary<Material, Color>();
+    public Color[] colors = new Color[3];
+    public Image[] buttonsImage = new Image[3];
+    public GameObject[] bg_Buttons = new GameObject[3];
+    int previousIdx = -1;
 
     void Awake()
     {
         if (instance == null) instance = this;
         else Destroy(gameObject);
+    }
+
+    public void IndicateSelectedColor(int num)
+    {
+        if(previousIdx != -1)
+        {
+            bg_Buttons[previousIdx].SetActive(false);
+        }
+        bg_Buttons[num].SetActive(true);
+    }
+
+    private void Start()
+    {
+        for(int i =0; i < 3; i++)
+        {
+            colors[i] = buttonsImage[i].color;
+        }
+        InitSaveMats();
+    }
+
+    void InitSaveMats()
+    {
+        mat_Rf1_Doors[0] = rf1_MeshRenderer.materials[1];
+        mat_Rf1_Doors[1] = rf1_MeshRenderer.materials[2];
+        mat_Rf1_Doors[2] = rf1_MeshRenderer.materials[11];
+        mat_Rf2_Doors[0] = rf2_MeshRenderer.materials[11];
+        mat_Rf2_Doors[1] = rf2_MeshRenderer.materials[5];
+        mat_Rf2_Doors[2] = rf2_MeshRenderer.materials[12];
+        mat_Rf2_Doors[3] = rf2_MeshRenderer.materials[1];
+        foreach(var c in mat_Rf1_Doors)
+        {
+            c.color = colors[0];
+            dic_DoorsColor.Add(c, c.color);
+        }
+        foreach (var c in mat_Rf2_Doors)
+        {
+            c.color = colors[0];
+            dic_DoorsColor.Add(c, c.color);
+        }
     }
 
     void Update()
@@ -57,13 +108,15 @@ public class K_UIManager : MonoBehaviour
 
     public void Enabled_UI()
     {
-        AccountDate.GetInstance().SetPlayerState(K_PlayerMove.PlayerState.Click);
         img_Aim.SetActive(false);
+        ui_ObjGuide.SetActive(false);
+        AccountDate.GetInstance().SetPlayerState(K_PlayerMove.PlayerState.Click);
     }
 
     public void Disabled_UI()
     {
-        AccountDate.GetInstance().SetPlayerState(K_PlayerMove.PlayerState.Move);
         img_Aim.SetActive(true);
+        ui_ObjGuide.SetActive(true);
+        AccountDate.GetInstance().SetPlayerState(K_PlayerMove.PlayerState.Move);
     }
 }
