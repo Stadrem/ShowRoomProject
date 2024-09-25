d82ee25e4fdff3b49df92f66a4e896d72421cd2f
KwonSiyoon <140003447+KwonSiyoon@users.noreply.github.com>
2024-09-25 13:56:35 +0900

    11

--- Assets/KSY/01.Scripts/K_UIController.cs
+++ Assets/KSY/01.Scripts/K_UIController.cs
@@ -7,20 +7,46 @@ public class K_UIController : MonoBehaviour
 {
     public Button[] btn_Panels;
     public GameObject[] panels;
-    GameObject previousPanel = null;
-    
+    int previousIndex = -1;
+    public GameObject virtualCam;
+
+    public ColorBlock normal;
+    public ColorBlock selected;
 
     void Start()
     {
+        normal = btn_Panels[0].colors;
+        selected = normal;
+        selected.normalColor = selected.selectedColor;
         btn_Panels[0].Select();
         btn_Panels[0].onClick.Invoke();
     }
 
+
     public void ActivePanel(int num)
     {
-        previousPanel?.SetActive(false);
+        if(previousIndex != -1)
+        {
+            panels[previousIndex].SetActive(false);
+            btn_Panels[previousIndex].colors = normal;
+        }
+        previousIndex = num;
         panels[num].SetActive(true);
-        previousPanel = panels[num];
+        btn_Panels[num].colors = selected;
+    }
+
+    private void OnEnable()
+    {
+        K_UIManager.GetInstance().Enabled_UI();
+        K_UIManager.GetInstance().OnCamFocusIn(virtualCam);
+        GameUiCanvas.instance.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        K_UIManager.GetInstance().Disabled_UI();
+        K_UIManager.GetInstance().OnCamFocusOut(virtualCam);
+        GameUiCanvas.instance.gameObject.SetActive(true);
     }
 
 }
---
+++ Assets/KSY/01.Scripts/K_UIManager.cs
@@ -0,0 +1,69 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class K_UIManager : MonoBehaviour
+{
+    private static K_UIManager instance;
+    public GameObject player;
+    public PhotonView playerPV;
+    public GameObject img_Aim;
+    public GameObject currentUI;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OpenUI()
+    {
+        currentUI.SetActive(true);
+    }
+
+    // 카메라 포커스 함수. 각 UIController에서 호출
+    public void OnCamFocusIn(GameObject virtualCam)
+    {
+        var cvc = virtualCam.GetComponent<CinemachineVirtualCamera>();
+        cvc.Priority = 11;
+    }
+
+    public void OnCamFocusOut(GameObject virtualCam)
+    {
+        var cvc = virtualCam.GetComponent<CinemachineVirtualCamera>();
+        cvc.Priority = 9;
+    }
+
+    public static K_UIManager GetInstance()
+    {
+        if(instance == null)
+        {
+            new GameObject("UIManager", typeof(K_UIManager));
+        }
+        return instance;
+    }
+
+    public void FindPlayer()
+    {
+        player = AccountDate.GetInstance().player;
+    }
+
+    public void Enabled_UI()
+    {
+        AccountDate.GetInstance().SetPlayerState(K_PlayerMove.PlayerState.Click);
+        img_Aim.SetActive(false);
+    }
+
+    public void Disabled_UI()
+    {
+        AccountDate.GetInstance().SetPlayerState(K_PlayerMove.PlayerState.Move);
+        img_Aim.SetActive(true);
+    }
+}
